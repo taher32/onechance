9a65c11df84d99573635247cab683f909c416378
taher32 <56154359+taher32@users.noreply.github.com>
2026-06-28 09:12:31 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function copyWallet() {
+
+    const wallet = "TQaYhadcedfPmm9i6TyAd2XVVwMqkbE81R";
+
+    navigator.clipboard.writeText(wallet);
+
+    alert("Wallet address copied successfully!");
+}
