056ce0fb01915c7d00ed69f43688b144e86f411c
taher32 <56154359+taher32@users.noreply.github.com>
2026-06-28 10:09:50 +0330

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function copyWallet() {
-
-    const wallet = "TQaYhadcedfPmm9i6TyAd2XVVwMqkbE81R";
-
-    navigator.clipboard.writeText(wallet);
-
-    alert("Wallet address copied successfully!");
-}
